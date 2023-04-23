e44b2d8d191b1e95fcf91e9c1a801aedb09e690a
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-04-23 16:12:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
-using namespace std;
+using namespace  std;
 
 int main() {
-   return 0;
+    int n, f, a=1, b=1;
+    cout << "inserire un numero intero\n";
+    cin >> n;
+    if (n<2){
+        cout << "errore\n";
+    }
+    else {
+
+    int c=0;
+            while (c<n){
+                c++;
+                f=a+b;
+                cout << f << endl;
+                b=a;
+                a=f;
+            }
+}
+    return 0;
 }
